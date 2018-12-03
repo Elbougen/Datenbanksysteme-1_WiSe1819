8ff58ba1230ac4a034dfca46c5a72e4343376416
Marvin <mpoenisch@hm.edu>
2018-12-03 12:56:30 +0100

    update prak 4 sql uberarbeitet Tabelle uberarbeitet

--- sql Quellcode/prak4Aufabe1d.sql
+++ sql Quellcode/prak4Aufabe1d.sql
@@ -1,56 +1,62 @@
 /* -------------- creat tables -------------- */
-CREATE TABLE person
-(
-    PersonalNr INTEGER NOT NULL PRIMARY KEY,
-    Vorname VARCHAR2(32 CHAR) NOT NULL,
-    Name VARCHAR2(32 CHAR) NOT NULL,
-	Lohnstufe VARCHAR2(32 CHAR) NOT NULL,
-	FunktionsNr INTEGER NOT NULL REFERENCES funktion (FunktionsNr)
-);
-
 CREATE TABLE funktion
 (
     FunktionsNr INTEGER NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE kurs
+CREATE TABLE person
 (
-    KursNr INTEGER NOT NULL PRIMARY KEY,
-	Bezeichnung VARCHAR2(32 CHAR) NOT NULL,
-	Ort VARCHAR2(32 CHAR) NOT NULL,
-	ThemenNr INTEGER NOT NULL REFERENCES ThemenNr (ThemenNr)
+	PersonalNr INTEGER NOT NULL PRIMARY KEY,
+	Vorname VARCHAR2(32 CHAR) NOT NULL,
+	Name VARCHAR2(32 CHAR) NOT NULL,
+	Lohnstufe VARCHAR2(32 CHAR) NOT NULL,
+	FunktionsNr INTEGER NOT NULL REFERENCES funktion (FunktionsNr)
 );
 
 CREATE TABLE kursthema 
 (
-    ThemenNr INTEGER NOT NULL PRIMARY KEY
+	ThemenNr INTEGER NOT NULL PRIMARY KEY
 );
+
+CREATE TABLE kurs
+(
+	KursNr INTEGER NOT NULL PRIMARY KEY,
+	Bezeichnung VARCHAR2(32 CHAR) NOT NULL,
+	Ort VARCHAR2(32 CHAR) NOT NULL,
+	ThemenNr INTEGER NOT NULL REFERENCES kursthema (ThemenNr)
+);
+
 CREATE TABLE fuer
 (
-    KursNr INTEGER NOT NULL PRIMARY KEY,
+	KursNr INTEGER NOT NULL PRIMARY KEY REFERENCES kurs (KursNr),
 	FunktionsNr INTEGER NOT NULL REFERENCES funktion (FunktionsNr)
 );
-CREATE TABLE internerkursleiter
-(
-    PersonalNr INTEGER NOT NULL PRIMARY KEY,
-	Dienstjahre INTEGER NOT NULL,
-	KursleiterNr INTEGER NOT NULL PRIMARY KEY REFERENCES kursleiter (KursleiterNr)
-);
+
 CREATE TABLE kursleiter
 (
-    KursleiterNr INTEGER NOT NULL PRIMARY KEY,
+	KursleiterNr INTEGER NOT NULL PRIMARY KEY,
 	Fachgebiet VARCHAR2(32 CHAR) NOT NULL
 );
+
+CREATE TABLE internerkursleiter
+(
+	KursleiterNr INTEGER NOT NULL PRIMARY KEY REFERENCES kursleiter (KursleiterNr),
+	PersonalNr INTEGER NOT NULL UNIQUE REFERENCES person (PersonalNr),
+	Dienstjahre INTEGER NOT NULL
+);
+
 CREATE TABLE externerKursleiter
 (
-    KursleiterNr INTEGER NOT NULL PRIMARY KEY REFERENCES kursleiter (KursleiterNr),
+	KursleiterNr INTEGER NOT NULL PRIMARY KEY REFERENCES kursleiter (KursleiterNr),
 	Vorname VARCHAR2(32 CHAR) NOT NULL,
 	Name VARCHAR2(32 CHAR) NOT NULL,
 	Firmenname VARCHAR2(32 CHAR) NOT NULL
 );
+
 CREATE TABLE kursteilnahme
 (
-    PersonalNr INTEGER NOT NULL PRIMARY KEY REFERENCES person (PersonalNr),
+	PersonalNr INTEGER NOT NULL PRIMARY KEY REFERENCES person (PersonalNr),
 	KursleiterNr INTEGER NOT NULL PRIMARY KEY REFERENCES kursleiter (KursleiterNr),
 	KursNr INTEGER NOT NULL PRIMARY KEY REFERENCES kurs (KursNr)
+	Kursdatum INTEGER NOT NULL
 );
